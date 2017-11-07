afc57d51583b8855b615fc937aa1188bdc7b9bb4
jerry <jerry_shenchao@163.com>
2017-11-07 20:53:53 +0800

    add new

---
+++ src/test/java/leetcode/P189Test.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class P189Test {
+
+    private P189 p189 = new P189();
+
+    @Test
+    public void test1() {
+        int[] nums = {1,2,3,4,5,6,7};
+        p189.rotate(nums, 3);
+        for (int n : nums) {
+            System.out.print(n + " ");
+        }
+    }
+}
