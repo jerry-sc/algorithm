909b80d6cb44530d493be9482ad47d767d782a43
jerry <jerry_shenchao@163.com>
2017-12-19 23:22:33 +0800

    add new

---
+++ src/main/java/leetcode/P687.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+import tree.TreeNode;
+
+public class P687 {
+
+    private int ans;
+
+    public int longestUnivaluePath(TreeNode root) {
+        ans = 0;
+        postOrder(root);
+        return ans;
+    }
+
+    private int postOrder(TreeNode root) {
+        if (root == null)
+            return 0;
+        int a = postOrder(root.left);
+        int b = postOrder(root.right);
+        int left = 0, right = 0;
+        if (root.left != null && root.left.val == root.val) {
+            left += a +1;
+        }
+        if (root.right != null && root.right.val == root.val) {
+            right += b + 1;
+        }
+        ans = Math.max(ans, left + right);
+        return Math.max(left, right);
+    }
+}
