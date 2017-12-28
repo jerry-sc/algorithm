80270f4e6239c79db73680d3ca2b9503ceaa1c57
jerry <jerry_shenchao@163.com>
2017-12-28 23:36:45 +0800

    add new

---
+++ src/main/java/leetcode/P107.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+import tree.TreeNode;
+
+import java.util.*;
+
+public class P107 {
+
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null)
+            return res;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            List<Integer> list = new ArrayList<>();
+            Queue<TreeNode> temp = new LinkedList<>();
+            while (!queue.isEmpty()) {
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if (node.left != null)
+                    temp.add(node.left);
+                if (node.right != null)
+                    temp.add(node.right);
+            }
+            queue.addAll(temp);
+            res.add(list);
+        }
+        Collections.reverse(res);
+        return res;
+    }
+}
