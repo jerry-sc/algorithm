bf612242ac5f1ef66aa6b5b7fb598d538347552f
jerry <jerry_shenchao@163.com>
2018-01-04 22:23:13 +0800

    add new

---
+++ src/main/java/leetcode/P121.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+public class P121 {
+
+    public int maxProfit(int[] prices) {
+        int max = 0;
+        int pre = 0;
+        for (int i = 1; i < prices.length; ++i) {
+            pre = Math.max(prices[i] - prices[i-1] + pre, 0);
+            max = Math.max(max, pre);
+        }
+        return max;
+    }
+}
