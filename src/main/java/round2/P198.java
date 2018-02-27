6a21bacc1bdf3b45ec526191865aeaeabd217392
jerry <jerry_shenchao@163.com>
2018-02-27 23:42:26 +0800

    add new

---
+++ src/main/java/round2/P198.java
@@ -0,0 +1,8 @@
+package round2;
+
+public class P198 {
+
+    public int rob(int[] nums) {
+
+    }
+}
---
+++ src/main/java/round2/P35.java
@@ -0,0 +1,17 @@
+package round2;
+
+public class P35 {
+
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length;
+        while (left < right) {
+            int mid =  left + (right - left) / 2;
+            if (nums[mid] >= target) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
